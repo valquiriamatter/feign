23124c7c53e772a576e5c052eedc432766ab9f66
valquiria matter <matter.valquiria@gmail.com>
2022-05-17 13:12:00 -0300

    ajustando

--- src/main/java/com/valquiriamatter/consumindoViaCep/service/EnderecoService.java
+++ src/main/java/com/valquiriamatter/consumindoViaCep/service/EnderecoService.java
@@ -2,13 +2,11 @@ package com.valquiriamatter.consumindoViaCep.service;
 
 import com.valquiriamatter.consumindoViaCep.cliente.EnderecoClient;
 import com.valquiriamatter.consumindoViaCep.modelo.Endereco;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EnderecoService {
 
-    @Autowired
     private final EnderecoClient enderecoClient;
 
     public EnderecoService(EnderecoClient enderecoClient) {
